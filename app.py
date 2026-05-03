49cc437bfa1d5d259426d3e7143317408edac8f1
memeqana-umsarah <meme.qana@gmail.com>
2026-05-03 09:49:37 +0300

    Fix database environment variable name in DB_CONFIG

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ DB_CONFIG = {
     "host": os.getenv("MYSQLHOST"),
     "user": os.getenv("MYSQLUSER"),
     "password": os.getenv("MYSQLPASSWORD"),
-    "database": os.getenv("MYSQL_DATABASE"),
+    "database": os.getenv("MYSQLDATABASE"),
     "port": int(os.getenv("MYSQLPORT")),
     "cursorclass": pymysql.cursors.DictCursor
 }
